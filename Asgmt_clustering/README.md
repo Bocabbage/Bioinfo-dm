ccaed962fb4e36f8c72f33fdb6986a7e25be55da
Bocabbage <378066505@qq.com>
2019-04-22 19:43:29 +0800

    Update at 04-22.

--- Asgmt_clustering/README.md
+++ Asgmt_clustering/README.md
@@ -1,13 +1,29 @@
 # Assignment-1: Do clustering for micro-array data
 
-## Target
+## 1.Target
 
 * Apply different cluster methods for micro-array Data
 * Compare the result of different methods
 
-## Data-Source
+## 2.Data-Source
 
 * Database: [GEO](https://www.ncbi.nlm.nih.gov/geo/)
 * Data: [GSE33532](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE33532)
         (25906 genes, 100 samples)
+
+## 3.Algorithms
+
+* [x]K-Means
+* [x]DBSCAN
+* [x]Hierarchical clustering
+* [ ]Spectral clustering
+
+Meet problem that 'SpectralClustering' take about 99% of my RAM(12GB), also spends a long time to run but 
+meet 'out of memory'. I just take 100 samples of the data and run them.
+
+![image](figs/4_clustering_04-22.png)
+
+And here is the time-cost performance of different algorithms:
+
+![image](figs/performance.png)
 
--- Asgmt_clustering/main.py
+++ Asgmt_clustering/main.py
@@ -1,4 +1,5 @@
 # 更新时间：2019/4/21(未完成)
+#          2019/4/22(增加层次聚类；谱聚类小样本test；未完成)
 
 import csv
 import numpy as np
@@ -8,7 +9,8 @@ from sklearn.decomposition import PCA
 
 from sklearn.cluster import KMeans
 from sklearn.cluster import DBSCAN
-from sklearn.cluster import spectral_clustering
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 
 # Set random seed
 random_state = 170
@@ -27,20 +29,45 @@ pca = PCA(n_components=3,svd_solver='full')
 Array_pca = pca.fit_transform(Array)
 #print(Array_pca.shape)
 
+# K-Mean:
 Kmeans_pre = KMeans(n_clusters=10,random_state=random_state).fit_predict(Array_pca)
+
+# DBSCAN:
 DBSCAN_pre = DBSCAN(eps=1,min_samples=1).fit_predict(Array_pca) 
+
+# Hierarchical clustering
+Agglom_pre = AgglomerativeClustering(n_clusters=10,
+                                     affinity="euclidean",
+                                     linkage="ward").fit_predict(Array_pca)
+
+# Spectral_clustering:
+SpecClus_pre = SpectralClustering(
+                n_clusters=10,
+                assign_labels='discretize',
+                random_state=random_state).fit_predict(Array_pca[0:100,:])
 
 
 fig = plt.figure()
 
 # KMeans result
-ax = fig.add_subplot(1,2,1,projection='3d')
+ax = fig.add_subplot(2,2,1,projection='3d')
 ax.scatter(Array_pca[:,0], Array_pca[:,1],Array_pca[:,2],c=Kmeans_pre)
 ax.set_title("KMeans: n_comp = 10")
 
 # DBSCAN result
-ax = fig.add_subplot(1,2,2,projection='3d')
+ax = fig.add_subplot(2,2,2,projection='3d')
 ax.scatter(Array_pca[:,0], Array_pca[:,1],Array_pca[:,2],c=DBSCAN_pre)
-ax.set_title("DBSCAN: eps=1,min_samples=3")
+ax.set_title("DBSCAN: eps=1,min_samples=1")
+
+# Hierarchical result
+ax = fig.add_subplot(2,2,3,projection='3d')
+ax.scatter(Array_pca[:,0], Array_pca[:,1],Array_pca[:,2],c=Agglom_pre)
+ax.set_title("Ward Linkage: n_comp = 10")
+
+# Spectral-Clustering result
+ax = fig.add_subplot(2,2,4,projection='3d')
+ax.scatter(Array_pca[0:100,0], Array_pca[0:100,1],Array_pca[0:100,2],c=SpecClus_pre)
+ax.set_title("Spectral-Clustering: n_comp=10(n_samples=100)")
+
 
 plt.show()
